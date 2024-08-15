a87ca7607d90ff9117a02c99eb91078ae3eee3e8
YordanovDY <yordanovdy23@gmail.com>
2024-08-15 17:45:37 +0300

    Adapt HTML and CSS Template

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
-import page from '../lib/page/page.mjs';
+import page from '@page/page.mjs';
+import { html, render } from '@lit/lit-html.js';
 
 page('/', () => console.log('Works!'));
 
 page.start();
+
+function greet(text) {
+    const main = document.querySelector('main');
+    const temp = () => html`<h1>${text}</h1>`
+
+    render(temp(), main);
+}
+
+greet('Test');
