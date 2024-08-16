43eb788a40a0f416ad8db35c0f5f81a1b36bc58d
YordanovDY <yordanovdy23@gmail.com>
2024-08-16 17:38:54 +0300

    Added logout functionality

--- src/middleware/navigation.js
+++ src/middleware/navigation.js
@@ -1,8 +1,9 @@
 import { html, render } from '@lit/lit-html.js';
 import { user } from '../utils/userUtils.js';
+import { userService } from '../service/userService.js';
 
 export function addNavControl() {
-    return function(ctx, next) {
+    return function (ctx, next) {
         ctx.updateNav = updateNav;
 
         next();
@@ -16,6 +17,7 @@ export function updateNav() {
     
         ${userData ? html`
             <li><a href="profile.html">Profile: ${userData.username}</a></li>
+            <li><a @click=${onLogout} href="javascript:void(0)">Logout</a></li>
         `: html`
             <li><a href="/login">Login</a></li>
             <li><a href="/register">Register</a></li>
@@ -26,4 +28,15 @@ export function updateNav() {
     const userData = user.getUserData();
     const nav = document.querySelector('nav');
     render(template(userData), nav);
+}
+
+async function onLogout() {
+    try {
+        await userService.logout();
+
+    } catch (error) {
+        return;
+    }
+
+    updateNav();
 }
